65b7b59201ea1eb5c5f8457e667fbac9a290688a
DElfinaMaldonado <del057677@gmail.com>
2025-01-30 13:30:40 -0600

    Cambios

--- python/input/input.py
+++ python/input/input.py
@@ -1,5 +1,10 @@
 #Pedirle un dato ala usuario
 nombre = input("dame tu nombre: ")
 
-#Mostrando el dato 
+#Mostrando el dato
 print(nombre)
+
+
+#Mostrando el dato
+#Utilizando f-strings
+print(f'el nombre es: {nombre}')
---
+++ python/input/inputnumeros.py
@@ -0,0 +1,8 @@
+#pedir un numero la usuario
+numero = input("dame un numero: ")
+
+#Multiplicando el numero por dos.
+resultado = numero*2
+
+#Mostrando el resultado
+print(resultado)
