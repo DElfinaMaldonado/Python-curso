41a805a9e8300784555a63b744a1ed62e58faf36
DElfinaMaldonado <del057677@gmail.com>
2025-02-05 15:34:48 -0600

    ejercicios

--- python/numpy/Segmentacionmatriz.py
+++ python/numpy/Segmentacionmatriz.py
@@ -13,3 +13,12 @@ print(arr[4:]) #Salida:[5,6,7]
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 print(arr[:4]) #Salida: [1,2,3,4]
+
+
+
+
+#Corte negativo #
+#Rebanada desde el índice 3 desde el final hasta el índice 1 desde el final:
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1]) #Salida: [5 6]
