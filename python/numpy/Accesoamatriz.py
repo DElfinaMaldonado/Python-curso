f659b87462fc65c876740d8bd2fe42556ab2b87a
DElfinaMaldonado <del057677@gmail.com>
2025-02-05 16:04:05 -0600

    quitar espacios

--- python/numpy/Accesoamatriz.py
+++ python/numpy/Accesoamatriz.py
@@ -9,8 +9,6 @@ arr = np.array([1, 2, 3, 4])
 print(arr[2] + arr[3]) #Salida: 7. ya que suma la posicion 2 y la 3 que es 2+3 =7
 
 
-
-
 #Acceso a matrices 2D
 import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
@@ -20,8 +18,6 @@ print('2do elemento en la 1ra fila: ', arr[0, 1]) #Salida 2
 import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('55to elemento en la 2da fila: ', arr[1, 4])
-
-
 
 #Indexación negativa #
 import numpy as np
